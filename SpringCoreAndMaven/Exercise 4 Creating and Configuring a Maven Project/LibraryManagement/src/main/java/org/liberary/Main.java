0aecc4754e07ef24c863c37585406c6c5cfd28a5
Himanshu Choudhary <chaudharyhimanshu276@gmail.com>
2026-07-09 10:37:12 +0530

    [Himanshu] Exercise 4: Creating and Configuring a Maven Project completed

---
+++ SpringCoreAndMaven/Exercise 4 Creating and Configuring a Maven Project/LibraryManagement/src/main/java/org/liberary/Main.java
@@ -0,0 +1,9 @@
+package org.liberary;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercise 4: Creating and Configuring a Maven Project");
+    }
+}

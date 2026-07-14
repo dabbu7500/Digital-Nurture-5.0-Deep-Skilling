2190df78bc4eed4a6c2880b5fc9be46199b0b3b0
Himanshu Choudhary <chaudharyhimanshu276@gmail.com>
2026-07-15 00:16:30 +0530

    [Himanshu] Spring REST using Spring Boot3 -hands on 6 Spring Core – Load list of countries from Spring Configuration XML  - Completed

---
+++ Spring REST using Spring Boot 3/1. spring-rest-handson/Hands on 6 Spring Core – Load list of countries from Spring Configuration XML/SpringCore_LoadCountryList/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,36 @@
+package com.cognizant.spring_learn;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        displayCountries();
+    }
+
+    public static void displayCountries() {
+
+        LOGGER.info("START");
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countries =
+                (List<Country>) context.getBean("countryList");
+
+        for (Country country : countries) {
+            LOGGER.info("{}", country);
+        }
+
+        context.close();
+
+        LOGGER.info("END");
+    }
+}
